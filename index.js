9ad2abffb812d12f633a6f9cfac5283ddbda0a8d
hananasif <58982990+hananasif@users.noreply.github.com>
2023-03-29 12:37:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const hostname = '127.0.0.1';
+const port = 3001;
+
+const userroute = require('./routes/router');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/user', userroute)
+
+app.listen(port, hostname, () => {
+    console.log(`Running At http://${hostname}:${port}/`);
+})
+
+
